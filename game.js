e05d113f12544ce5f625fc12058e9cd4ef259ede
Bhavya Bibi <bhavyabibi007@gmail.com>
2021-02-24 17:12:32 +0400

    snakeBox Created

--- game.js
+++ game.js
@@ -1,2 +1,8 @@
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
+
+const canvasSize = 600;
+canvas.Width = canvasSize;
+canvas.height = canvasSize;
+const snakeBox = 20;
+const totalMoves = canvasSize / snakeBox;
