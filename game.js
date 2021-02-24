a83f0d1e39a3bd5af707a51d08e850a40826d06f
Bhavya Bibi <bhavyabibi007@gmail.com>
2021-02-24 17:15:30 +0400

    apple is defined

--- game.js
+++ game.js
@@ -4,5 +4,9 @@ const ctx = canvas.getContext("2d")
 const canvasSize = 600;
 canvas.Width = canvasSize;
 canvas.height = canvasSize;
+
 const snakeBox = 20;
 const totalMoves = canvasSize / snakeBox;
+
+const apple = new Image();
+apple.src = "images/apple.png";
